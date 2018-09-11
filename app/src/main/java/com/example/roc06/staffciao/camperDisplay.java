0d54c53133a0288806433d2cdf1df0564954654a
Robert <roc0614@gmail.com>
2018-09-11 18:31:51 -0400

    Added Safeword and Guardian information to Camper class, and outputs to display in the Camper view

--- app/src/main/java/com/example/roc06/staffciao/camperDisplay.java
+++ app/src/main/java/com/example/roc06/staffciao/camperDisplay.java
@@ -3,7 +3,12 @@ package com.example.roc06.staffciao;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class camperDisplay extends AppCompatActivity {
 
@@ -20,7 +25,7 @@ public class camperDisplay extends AppCompatActivity {
         temp.setText(camper.getName());
         temp = findViewById(R.id.camperAge_TextView);
         String age = String.valueOf(camper.age);
-        temp.setText(age);
+        temp.setText(age + " Years Old");
         temp = findViewById(R.id.camperCourse_TextView);
         temp.setText(camper.courseName);
         temp = findViewById(R.id.camperLunchType_TextView);
@@ -28,6 +33,16 @@ public class camperDisplay extends AppCompatActivity {
         temp.setText(tempText);
         temp = findViewById(R.id.camperShirtSize_TextView);
         temp.setText(camper.tShirtSize);
+
+        LinearLayout guardianLayout = findViewById(R.id.guardianLinearLayout);
+        for(int i = 0; i < camper.guardianNames.length; i++) {
+            LinearLayout tempLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.guardian_contact_info_layout_view, null) ;
+            guardianLayout.addView(tempLayout);
+            TextView tempView = (TextView) tempLayout.getChildAt(0);
+            tempView.setText(camper.guardianNames[i] + ":");
+            tempView = (TextView) tempLayout.getChildAt(2);
+            tempView.setText(camper.guardianContactNumbers[i]);
+        }
 
     }
 }
